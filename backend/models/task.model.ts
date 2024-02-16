79761f58371c6fa2f12a85e8f8186c1205eaa0ad
Aathithan Chandrabalan <a69chand@uwaterloo.ca>
2024-02-15 19:51:10 -0500

    init

---
+++ backend/models/task.model.ts
@@ -0,0 +1,48 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+import { TaskStatus } from "../types";
+
+export interface Task extends Document {
+  id: string;
+  title: string;
+  description: string;
+  requiresApproval: boolean;
+  status: TaskStatus;
+  document: string;
+  dueDate: Date;
+  expiry: Date;
+}
+
+const TaskSchema: Schema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  requiresApproval: {
+    type: Boolean,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ["Pending", "Completed", "Approved"],
+  },
+  document: {
+    type: String,
+    required: true,
+  },
+  dueDate: {
+    type: Date,
+    required: true,
+  },
+  expiry: {
+    type: Date,
+    required: true,
+  },
+});
+
+export default mongoose.model<Task>("Task", TaskSchema);
