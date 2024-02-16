188d5c418995c8fc7f7128063871ec499d76474a
Carolyn Zhang <82423082+carolynzhang18@users.noreply.github.com>
2024-02-15 19:10:58 -0500

    Email authentication (#17)
    * Move graphql operations to separate folder in frontend
    
    * Add check for whether user is verified
    
    * Rename frontend graphql files
    
    * Run linter
    
    * Fix imports
    
    * Add error message for unverified user log in

--- frontend/src/APIClients/AuthAPIClient.ts
+++ frontend/src/APIClients/AuthAPIClient.ts
@@ -102,6 +102,9 @@ const register = async (
     });
     user = result.data?.register ?? null;
     if (user) {
+      window.alert(
+        "Success! Please check your email for a link to verify your account.",
+      );
       localStorage.setItem(AUTHENTICATED_USER_KEY, JSON.stringify(user));
     }
   } catch (e: unknown) {
