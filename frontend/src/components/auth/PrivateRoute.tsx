e0dcbabae0ed7f184c5d4e383c27dbc1d03d193b
Carolyn Zhang <82423082+carolynzhang18@users.noreply.github.com>
2024-02-26 17:10:07 -0500

    Add private route role restrictions (#26)
    * Add private route role restrictions
    
    * Revert to allowedRoles

--- frontend/src/components/auth/PrivateRoute.tsx
+++ frontend/src/components/auth/PrivateRoute.tsx
@@ -2,26 +2,37 @@ import React, { useContext } from "react";
 import { Route, Redirect } from "react-router-dom";
 
 import AuthContext from "../../contexts/AuthContext";
-import { LOGIN_PAGE } from "../../constants/Routes";
+import { UserRole } from "../../types/UserTypes";
+import { HOME_PAGE, LOGIN_PAGE } from "../../constants/Routes";
 
 type PrivateRouteProps = {
   component: React.FC;
   path: string;
   exact: boolean;
+  allowedRoles?: Set<UserRole>; // if undefined, the default is all roles are allowed
 };
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({
   component,
   exact,
   path,
+  allowedRoles,
 }: PrivateRouteProps) => {
   const { authenticatedUser } = useContext(AuthContext);
+  if (!authenticatedUser) {
+    return <Redirect to={LOGIN_PAGE} />;
+  }
 
-  return authenticatedUser ? (
-    <Route path={path} exact={exact} component={component} />
-  ) : (
-    <Redirect to={LOGIN_PAGE} />
-  );
+  const userHasAllowedRole = allowedRoles
+    ? allowedRoles.has(authenticatedUser.role)
+    : true;
+  if (!userHasAllowedRole) {
+    alert(
+      "You don't have permission to access this page. Redirecting to home page.",
+    );
+    return <Redirect to={HOME_PAGE} />;
+  }
+  return <Route path={path} exact={exact} component={component} />;
 };
 
 export default PrivateRoute;
