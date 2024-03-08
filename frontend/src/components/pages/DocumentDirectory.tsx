9b7ed8212109ad7d5167181aa7d574e5cfca333c
Carolyn Zhang <carolyn.zhang18@gmail.com>
2024-03-08 18:34:15 -0500

    Create document directory table with checkboxes

--- frontend/src/components/pages/DocumentDirectory.tsx
+++ frontend/src/components/pages/DocumentDirectory.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import DocumentDirectoryTable from "../tables/DocumentDirectoryTable";
 
 const DocumentDirectory = (): React.ReactElement => {
-  return <h1>Document Directory Page</h1>;
+  return (
+    <>
+      <h1>Document Directory Page</h1>
+      <DocumentDirectoryTable />
+    </>
+  );
 };
 export default DocumentDirectory;
