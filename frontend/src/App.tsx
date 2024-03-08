c0b85d60d0ea787ec3d31c8e5e28e67c16e134c5
itsjanevvu <86447206+itsjanevvu@users.noreply.github.com>
2024-03-07 21:26:40 -0500

    Create Document Directory (#31)
    * Create volunteer, staff and admin roles
    
    * Fix default roles
    
    * Update role in unit test files
    
    * Update lint
    
    * Update table name
    
    * Create document directory page
    
    * Add return function
    
    * Remove extra import statement
    
    * Add route for SimpleEntityUpdatePage back in
    
    ---------
    
    Co-authored-by: Owen Sellner <43042601+owen-sellner@users.noreply.github.com>
    Co-authored-by: Owen Sellner <owen.sellner@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,6 +24,7 @@ import sampleContextReducer from "./reducers/SampleContextReducer";
 import SampleContextDispatcherContext from "./contexts/SampleContextDispatcherContext";
 import EditTeamInfoPage from "./components/pages/EditTeamPage";
 import HooksDemo from "./components/pages/HooksDemo";
+import DocumentDirectory from "./components/pages/DocumentDirectory";
 
 import { AuthenticatedUser } from "./types/AuthTypes";
 import { UserRole } from "./types/UserTypes";
@@ -84,6 +85,12 @@ const App = (): React.ReactElement => {
                 exact
                 path={Routes.UPDATE_SIMPLE_ENTITY_PAGE}
                 component={SimpleEntityUpdatePage}
+              />
+              <PrivateRoute
+                exact
+                path={Routes.DOCUMENT_DIRECTORY_PAGE}
+                component={DocumentDirectory}
+                allowedRoles={new Set([UserRole.ADMIN])}
               />
               <PrivateRoute
                 exact
